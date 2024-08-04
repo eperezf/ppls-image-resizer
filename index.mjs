53bd3e265279417fda06a2e5af863122d94e77ab
Eduardo Pérez <eduardojoseperezf@gmail.com>
2024-08-04 17:29:48 -0400

    Refactor image resizing logic and support multiple resolutions

--- index.mjs
+++ index.mjs
@@ -20,25 +20,24 @@ const S3 = new S3Client({
 });
 
 const resolutions = [
-	{width: 100, height: 100},
-	{width: 1024, height: 540},
-	{width: 1155, height: 760},
-	{width: 1170, height: 585},
-	{width: 1400, height: 739},
-	{width: 1440, height: 750},
-	{width: 293, height: 155},
-	{width: 293, height: 293},
-	{width: 300, height: 158},
-	{width: 370, height: 247},
-	{width: 370, height: 490},
-	{width: 390, height: 390},
-	{width: 585, height: 293},
-	{width: 585, height: 585},
-	{width: 585, height: 760},
-	{width: 770, height: 406},
-	{width: 770, height: 513},
-	{width: 770, height: 760},
-	{width: 900, height: 760},
+	{ width: 370, height: 247 }, // 33% Landscape
+	{ width: 585, height: 293 }, // 50% Landscape Half Height
+	{ width: 770, height: 513 }, // 66% Landscape
+	{ width: 1155, height: 770 }, // 100% Landscape
+	{ width: 1170, height: 585 }, // Screen Ladnscape Small
+	{ width: 1500, height: 750 }, // Screen Landscape
+	{ width: 293, height: 293 }, // 25% Square
+	{ width: 390, height: 390 }, // 33% Square
+	{ width: 585, height: 585 }, // 50% Square
+	{ width: 900, height: 900 }, // 66% Square
+	{ width: 370, height: 490 }, // 33% Portrait
+	{ width: 585, height: 775 }, // 50% Portrait
+	{ width: 770, height: 1020 }, // 66% Portrait
+	
+	{ width: 293, height: null }, // 25%
+	{ width: 770, height: null }, // 50%
+	{ width: 1400, height: null }, // 66%
+	
 ]
 
 const allowedExtensions = [
@@ -86,29 +85,36 @@ export const handler = async (event, context) => {
 		}
 	}
 	console.timeEnd("R2 Get Object")
+	console.time("Resize and upload")
 	const imageByteArray = await res.Body.transformToByteArray()
 	const image = sharp(imageByteArray)
 	const imageMetadata = await image.metadata()
-	console.time("Resize and upload")
+	//console.log(imageMetadata);
 	await Promise.all(resolutions.map(async (resolution) => {
-		let resized = await image
+		// Do not oversize
+		if (resolution.width < imageMetadata.width) {
+			let resized = await image
 			.resize(resolution.width, resolution.height)
 			.jpeg({ force: false, quality: 80, chromaSubsampling: imageMetadata.chromaSubsampling })
 			.png({ force: false, quality: 80 })
 			.toBuffer();
-		
-		let data = {
-			Key: `${imageFilename}-${resolution.width}x${resolution.height}${imageExtension}`,
-			Body: resized,
-			ContentType: `image/${imageMetadata.format}`,
-		};
-		let putCommand = new PutObjectCommand({Bucket: process.env.BUCKET_NAME, ...data})
-		try {
-			let putResponse = await S3.send(putCommand)
-			//console.log(putResponse);
-		} catch (error) {
-			console.log(error)
-			return 0
+			let newImageMetadata = await sharp(resized).metadata()
+			let data = {
+				Key: `${imageFilename}-${newImageMetadata.width}x${newImageMetadata.height}${imageExtension}`,
+				Body: resized,
+				ContentType: `image/${imageMetadata.format}`,
+			};
+			let putCommand = new PutObjectCommand({Bucket: process.env.BUCKET_NAME, ...data})
+			//console.log(data);
+			try {
+				let putResponse = await S3.send(putCommand)
+				//console.log(putResponse);
+			} catch (error) {
+				console.log(error)
+				return 0
+			}
+		} else {
+			//console.log(`Skipping resolution ${resolution.width}x${resolution.height} as it is larger than the original image`)
 		}
 	}));
 	console.timeEnd("Resize and upload")
